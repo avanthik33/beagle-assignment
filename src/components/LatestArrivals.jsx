0feeb40ba013f6ce7b941cb1d5b3c08bd5ec1a40
avanthik33 <avanthik33@gmail.com>
2024-07-01 21:10:20 +0530

    completed letestarrivals

--- src/components/LatestArrivals.jsx
+++ src/components/LatestArrivals.jsx
@@ -1,15 +1,18 @@
 import React from "react";
+import "../App.css";
 
 const LatestArrivals = () => {
   const imageStyle = {
-    width: "350px",
-    height: "300px",
+    width: "500px",
+    height: "270px",
   };
   return (
     <div>
-      <div className="container mt-5 mb-5">
+      <div className="container mt-5" style={{
+        marginBottom:"30px"
+      }}>
         <div className="row">
-          <h1>OUR LATEST ARRIVALS</h1>
+          <h1 className="mainHeading">OUR LATEST ARRIVALS</h1>
         </div>
         <div className="row g-3">
           <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
@@ -17,31 +20,34 @@ const LatestArrivals = () => {
               <div className="col col-12 col-sm-4 col-md-4 col-lg-4 col-xl-4 col-xxl-4">
                 <div className="row">
                   <img
+                    style={imageStyle}
                     src={`${process.env.PUBLIC_URL}/images/toycar1.jpg`}
                     alt="img1"
                   />
-                  <h4>Wooden Car</h4>
-                  <p>100$</p>
+                  <h4 className="productTitle">Toy Car</h4>
+                  <p className="productPrice">190$</p>
                 </div>
               </div>
               <div className="col col-12 col-sm-4 col-md-4 col-lg-4 col-xl-4 col-xxl-4">
                 <div className="row">
                   <img
+                    style={imageStyle}
                     src={`${process.env.PUBLIC_URL}/images/toy2.jpg`}
                     alt="img2"
                   />
-                  <h4>Wooden Car</h4>
-                  <p>100$</p>
+                  <h4 className="productTitle">Spinner Toy</h4>
+                  <p className="productPrice">70$</p>
                 </div>
               </div>
               <div className="col col-12 col-sm-4 col-md-4 col-lg-4 col-xl-4 col-xxl-4">
                 <div className="row">
                   <img
-                    src="https://5.imimg.com/data5/RA/HF/MY-3473550/electrical-baby-toy-car.jpg"
+                    style={imageStyle}
+                    src={`${process.env.PUBLIC_URL}/images/toy3.jpg`}
                     alt="img3"
                   />
-                  <h4>Wooden Car</h4>
-                  <p>100$</p>
+                  <h4 className="productTitle">Dinosaur Toys</h4>
+                  <p className="productPrice">160$</p>
                 </div>
               </div>
             </div>
